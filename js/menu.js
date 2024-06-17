ad535a8aa873541dddd2812248914c67edebaf60
Martin Kozák <kozak@mivio.sk>
2024-06-18 00:45:19 +0200

    upravy nacitania dna a pdola toho spravny tab dňa, kluc cez in JS subor

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,50 @@
+import { myKluc } from './constant.js';
 
 //načítanie menu
-const kluc = '';
+const kluc = myKluc;
 const spreadsheetId = '1dgnH_IIQG_qG50bNb7XZ0_2Fzsl5aO8QdZEF7NxkRuY';
 const range = 'menuLuna!A1:D42'; // Adjust the range as needed
+
+//=============== zobrazenie dňa
+ const currentDate = new Date();
+ const den = 1;
+ //currentDate.getDay();
+ 
+ if (den === 2) {
+     // odznačenie aktívnosti
+     document.getElementById('tab_1').classList.remove('active');
+     document.getElementById('tab_first1').classList.remove('active', 'show');
+    // aktívnosť pre 2. deň
+     document.getElementById('tab_2').classList.add('active');
+     document.getElementById('tab_first2').classList.add('active', 'show');
+ }
+ if (den === 3) {
+    // odznačenie aktívnosti
+    document.getElementById('tab_1').classList.remove('active');
+    document.getElementById('tab_first1').classList.remove('active', 'show');
+   // aktívnosť pre 2. deň
+    document.getElementById('tab_3').classList.add('active');
+    document.getElementById('tab_first3').classList.add('active', 'show');
+}
+if (den === 4) {
+    // odznačenie aktívnosti
+    document.getElementById('tab_1').classList.remove('active');
+    document.getElementById('tab_first1').classList.remove('active', 'show');
+   // aktívnosť pre 2. deň
+    document.getElementById('tab_4').classList.add('active');
+    document.getElementById('tab_first4').classList.add('active', 'show');
+}
+if (den === 5) {
+    // odznačenie aktívnosti
+    document.getElementById('tab_1').classList.remove('active');
+    document.getElementById('tab_first1').classList.remove('active', 'show');
+   // aktívnosť pre 2. deň
+    document.getElementById('tab_5').classList.add('active');
+    document.getElementById('tab_first5').classList.add('active', 'show');
+}
+//=============== zobrazenie dňa END
+
+
 
 fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${kluc}`)
     .then(response => response.json())
@@ -20,7 +62,7 @@ fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${r
         document.getElementById('po-p2x').innerHTML = m[3][2] + " Al: " + m[3][1];
         document.getElementById('po-m1').innerHTML = m[4][0];
         document.getElementById('po-m2').innerHTML = m[5][0];
-        console.log("Menu:", m)    
+        //console.log("Menu:", m)    
     })
 .catch(error => console.error('Error fetching data:', error));
 
