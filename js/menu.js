1fb064c13b8c9a3544e3e95367b163b594d29805
Martin Kozák <kozak@mivio.sk>
2024-06-30 23:02:38 +0200

    oprava menu

--- js/menu.js
+++ js/menu.js
@@ -75,62 +75,62 @@ fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${r
         //utorok
         //polievky
         document.getElementById('ut01').innerHTML = m[9][0];
-        document.getElementById('ut02').innerHTML = m[9][2] + ", Al: " + m[2][1];
+        document.getElementById('ut02').innerHTML = m[9][2] + ", Al: " + m[9][1];
         document.getElementById('ut03').innerHTML = m[10][0];
-        document.getElementById('ut04').innerHTML = m[10][2] + ", Al: " + m[3][1];
+        document.getElementById('ut04').innerHTML = m[10][2] + ", Al: " + m[10][1];
         //menu1
         document.getElementById('ut05').innerHTML = m[11][0];
         document.getElementById('ut06').innerHTML = m[11][3];
-        document.getElementById('ut07').innerHTML = m[11][2] + ", Al: " + m[4][1];
+        document.getElementById('ut07').innerHTML = m[11][2] + ", Al: " + m[11][1];
         //menu2
         document.getElementById('ut08').innerHTML = m[12][0];
         document.getElementById('ut09').innerHTML = m[12][3];
-        document.getElementById('ut10').innerHTML = m[12][2] + ", Al: " + m[5][1];
+        document.getElementById('ut10').innerHTML = m[12][2] + ", Al: " + m[12][1];
 
         //streda
         //polievky
         document.getElementById('st01').innerHTML = m[16][0];
-        document.getElementById('st02').innerHTML = m[16][2] + ", Al: " + m[2][1];
+        document.getElementById('st02').innerHTML = m[16][2] + ", Al: " + m[16][1];
         document.getElementById('st03').innerHTML = m[17][0];
-        document.getElementById('st04').innerHTML = m[17][2] + ", Al: " + m[3][1];
+        document.getElementById('st04').innerHTML = m[17][2] + ", Al: " + m[17][1];
         //menu1
         document.getElementById('st05').innerHTML = m[18][0];
         document.getElementById('st06').innerHTML = m[18][3];
-        document.getElementById('st07').innerHTML = m[18][2] + ", Al: " + m[4][1];
+        document.getElementById('st07').innerHTML = m[18][2] + ", Al: " + m[18][1];
         //menu2
         document.getElementById('st08').innerHTML = m[19][0];
         document.getElementById('st09').innerHTML = m[19][3];
-        document.getElementById('st10').innerHTML = m[19][2] + ", Al: " + m[5][1];
+        document.getElementById('st10').innerHTML = m[19][2] + ", Al: " + m[19][1];
 
         //štvrtok
         //polievky
         document.getElementById('sv01').innerHTML = m[23][0];
-        document.getElementById('sv02').innerHTML = m[23][2] + ", Al: " + m[2][1];
+        document.getElementById('sv02').innerHTML = m[23][2] + ", Al: " + m[23][1];
         document.getElementById('sv03').innerHTML = m[24][0];
-        document.getElementById('sv04').innerHTML = m[24][2] + ", Al: " + m[3][1];
+        document.getElementById('sv04').innerHTML = m[24][2] + ", Al: " + m[24][1];
         //menu1
         document.getElementById('sv05').innerHTML = m[25][0];
         document.getElementById('sv06').innerHTML = m[25][3];
-        document.getElementById('sv07').innerHTML = m[25][2] + ", Al: " + m[4][1];
+        document.getElementById('sv07').innerHTML = m[25][2] + ", Al: " + m[25][1];
         //menu2
         document.getElementById('sv08').innerHTML = m[26][0];
         document.getElementById('sv09').innerHTML = m[26][3];
-        document.getElementById('sv10').innerHTML = m[26][2] + ", Al: " + m[5][1];
+        document.getElementById('sv10').innerHTML = m[26][2] + ", Al: " + m[26][1];
 
         //piatok
         //polievky
         document.getElementById('pi01').innerHTML = m[30][0];
-        document.getElementById('pi02').innerHTML = m[30][2] + ", Al: " + m[2][1];
+        document.getElementById('pi02').innerHTML = m[30][2] + ", Al: " + m[30][1];
         document.getElementById('pi03').innerHTML = m[31][0];
-        document.getElementById('pi04').innerHTML = m[31][2] + ", Al: " + m[3][1];
+        document.getElementById('pi04').innerHTML = m[31][2] + ", Al: " + m[31][1];
         //menu1
         document.getElementById('pi05').innerHTML = m[32][0];
         document.getElementById('pi06').innerHTML = m[32][3];
-        document.getElementById('pi07').innerHTML = m[32][2] + ", Al: " + m[4][1];
+        document.getElementById('pi07').innerHTML = m[32][2] + ", Al: " + m[32][1];
         //menu2
         document.getElementById('pi08').innerHTML = m[33][0];
         document.getElementById('pi09').innerHTML = m[33][3];
-        document.getElementById('pi10').innerHTML = m[33][2] + ", Al: " + m[5][1];
+        document.getElementById('pi10').innerHTML = m[33][2] + ", Al: " + m[33][1];
         
         
         //Trvalé menu 1
